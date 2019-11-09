bac962cb4145dac73f0fe862f30428a6361b6d29
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-11-08 17:51:51 -0800

    Created firebase database to persist state for each store visited

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,21 @@ import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
     state = {
         fishes: {},
         order: {}
+    }
+
+    componentDidMount() {
+        const { params } = this.props.match;
+        this.ref = base.syncState(`${params.storeId}/fishes`, {context: this, state: 'fishes'});
+    }
+
+    componentWillUnmount() {
+        base.removeBinding(this.ref);
     }
     
     addFish = (fish) => {
@@ -16,7 +26,7 @@ class App extends React.Component {
         fishes[`fish${Date.now()}`] = fish;
         this.setState({fishes});
     }
-
+ 
     loadSampleFishes = () => {
         this.setState({fishes : sampleFishes});
     }
