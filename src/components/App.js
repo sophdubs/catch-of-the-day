acf0ef57e4bd1e151cd1b4d5e4558d3fe657f18d
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-11-11 17:49:31 -0800

    Live state editing using editFishForm and updateFish

--- src/components/App.js
+++ src/components/App.js
@@ -34,6 +34,15 @@ class App extends React.Component {
         fishes[`fish${Date.now()}`] = fish;
         this.setState({fishes});
     }
+
+    updateFish = (key, updatedFish) => {
+        //1.Take copy of current state
+        const fishes = this.state.fishes;
+        //2.Update that state
+        fishes[key] = updatedFish;
+        //3.Set that to state
+        this.setState({ fishes });
+    }
  
     loadSampleFishes = () => {
         this.setState({fishes : sampleFishes});
@@ -59,7 +68,7 @@ class App extends React.Component {
                     </ul>
                 </div>
                 <Order fishes={this.state.fishes} order={this.state.order}/>  
-                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+                <Inventory fishes={this.state.fishes} addFish={this.addFish} updateFish={this.updateFish} loadSampleFishes={this.loadSampleFishes}/>
             </div>
         );
     }
