397a913bac11f5667d89aeb17e7e855caeda6777
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-11-11 16:38:27 -0800

    Persisting order state using local storage. fixed minor bug in Order.js because Fish are being loaded from DB and order is being loaded from local storage. Now if no fish (fish not loaded yet) we return null

--- src/components/Order.js
+++ src/components/Order.js
@@ -6,6 +6,7 @@ class Order extends React.Component {
         const fish = this.props.fishes[key];
         const count = this.props.order[key];
         const isAvailable = fish && fish.status === 'available';
+        if (!fish) return null;
 
         if (!isAvailable){
             return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>
